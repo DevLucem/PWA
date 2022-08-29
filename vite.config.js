ad24b25010e8e6a81ca9bfae573b71dc6cca54b8
Lucem Anb <lucematopz@gmail.com>
2022-08-29 14:11:38 +0300

    Git Pages Test

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
+    outDir: './docs',
     rollupOptions: {
       output: {
         entryFileNames: `[name].js`,
